20cfacca68d9b28f869bca289f4bc00ecba6ff3a
mozibrishu <mozib.diu@gmail.com>
2021-11-18 16:41:44 +0600

    Most of the part complete

---
+++ script.js
@@ -0,0 +1,56 @@
+const inputBox = document.querySelector(".inputField input");
+const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+showTasks();
+inputBox.onkeyup = () => {
+    let userInput = inputBox.value;
+    if (userInput.trim().length != 0){
+        addBtn.classList.add("active");
+    }
+    else{
+        addBtn.classList.remove("active");
+    }
+}
+
+addBtn.onclick = () => {
+    let userInput = inputBox.value;
+    let getLocalStorage = localStorage.getItem("new todo");
+    if(getLocalStorage == null){
+        listArr = [];
+    }
+    else{
+        listArr = JSON.parse(getLocalStorage);
+    }
+    listArr.push(userInput);
+    localStorage.setItem("new todo", JSON.stringify(listArr));
+    showTasks();
+}
+
+// showing tasklist
+function showTasks(){
+    let getLocalStorage = localStorage.getItem("new todo");
+    if(getLocalStorage == null){
+        listArr = [];
+    }
+    else{
+        listArr = JSON.parse(getLocalStorage);
+    }
+
+    let newLiTag = '';
+    const pendingTaskNum = document.querySelector(".pendingTaskNum");
+    pendingTaskNum.textContent = listArr.length;
+    listArr.forEach((elm,index) => {
+        newLiTag += `<li> ${elm}<span onclick="deleteTask(${index})"><i class="fa fa-trash"></i></span></li>`
+    });
+    todoList.innerHTML = newLiTag;
+    inputBox.value = "";
+}
+
+// delete task
+function deleteTask(index){
+    let getLocalStorage = localStorage.getItem("new todo");
+    listArr = JSON.parse(getLocalStorage);
+    listArr.splice(index, 1);
+    localStorage.setItem("new todo", JSON.stringify(listArr));
+    showTasks();
+}
